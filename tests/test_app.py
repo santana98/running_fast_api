6a76edf9cf6169bca4dc8a20ea9949b7fe5b1c43
Elias Santana <eliasvssantana@gmail.com>
2024-06-22 19:22:30 -0300

    starting the project

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from running_fast_api.app import app
+
+
+def test_read_root_return_ok_and_hello():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello, world!'}
